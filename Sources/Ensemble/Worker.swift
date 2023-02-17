abe89577c6f31915c9273761d146a873a5a0ff9e
Cole Roberts <cole.alan.roberts@gmail.com>
2023-02-17 14:48:54 -0600

    fix acl

--- Sources/Ensemble/Worker.swift
+++ Sources/Ensemble/Worker.swift
@@ -10,8 +10,24 @@ import Foundation
 // MARK: - `Worker` -
 
 /// A box type for managing asynchronous work
-public struct Worker<Reducer: Reducing> {
+public struct Worker<Reducer: Reducing>: Equatable {
+    
+    // MARK: - `Private Properties` -
+    
+    private let id = UUID()
+    
+    // MARK: - `Public` - 
     
     /// The operation to run, returning an `Action` from the inferred `Reducer`
-    let run: () async throws -> Reducer.Action?
+    public let run: () async throws -> Reducer.Action?
+    
+    // MARK: - `Init` -
+    
+    public init(run: @escaping () async throws -> Reducer.Action?) {
+        self.run = run
+    }
+    
+    public static func == (lhs: Worker<Reducer>, rhs: Worker<Reducer>) -> Bool {
+        lhs.id == rhs.id
+    }
 }
